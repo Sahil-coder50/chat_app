13228a391dda1ff9d36c6311130ea854292559d8
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-06-23 12:32:07 +0000

    Updated Serializers

--- modules/chats/serializers/conversation_serializer.py
+++ modules/chats/serializers/conversation_serializer.py
@@ -2,9 +2,29 @@
 from rest_framework import serializers
 
 from chats.models import Conversation
-from chats.serializers import MessageSerializer
+
+class ConversationListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Conversation
+        fields = "__all__"
+
 
 class ConversationRetrieveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Conversation
+        fields = "__all__"
+
+
+class ConversationCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Conversation
+        fields = "__all__"
+
+
+class ConversationUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Conversation
--- modules/chats/serializers/message_serializer.py
+++ modules/chats/serializers/message_serializer.py
@@ -3,7 +3,28 @@ from rest_framework import serializers
 
 from chats.models import Message
 
-class MessageSerializer(serializers.ModelSerializer):
+class MessageListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class MessageRetrieveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class MessageCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+    
+class MessageUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Message
